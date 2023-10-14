150b70aa414710b187cb5f3e97a497b25f5d2180
Abdelrhmanmatouk <abdomatouk97@gmail.com>
2023-10-15 00:05:11 +0300

    thid commit

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -11,7 +11,7 @@ export class CheckoutService {
 
 
   onlineCheckout(cartid:string,shipingAddress:any):Observable<any>{
-return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/orders/checkout-session/'+cartid+'?url=https://e-commerce-pi-rust.vercel.app/#/',{
+return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/orders/checkout-session/'+cartid+'?url=https://e-commerce-pi-rust.vercel.app/#/allorders',{
 shipingAddress
 },{
   headers:{
